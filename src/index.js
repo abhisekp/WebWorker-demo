1519fa8c1a2d92b5ab2f515f920bea3a691eca4f
Abhisek Pattnaik <abhisekp@engineer.com>
2016-02-02 17:29:00 +0530

    Create app entry main file

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './css/app';
+console.log('Initiated');
+import {postMessage, getMessage} from './js/worker';
+
+document.addEventListener('DOMContentLoaded', init);
+
+let workerBlob;
+require(['raw!../builds/worker'], (val) => {
+    workerBlob = new Blob([val], {type: 'text/javascript'});
+    console.log(workerBlob);
+});
+
+function init() {
+    document.querySelector('.btn').addEventListener('click', (ev) => {
+        ev.preventDefault();
+        console.log('Clicked');
+
+        if(!workerBlob) {
+            return;
+        }
+        const workerURL = URL.createObjectURL(workerBlob);
+        const worker = new Worker(workerURL);
+        URL.revokeObjectURL(workerURL);
+
+        worker.addEventListener('message', (msgEv) => {
+            const data = getMessage(msgEv);
+            console.log('Long Running Process result:', data);
+        });
+        postMessage(worker, 'GO');
+    }, false);
+
+    document.addEventListener('click', () => console.log('Doc clicked'));
+}
